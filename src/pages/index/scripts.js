14a1cbd03cc2be252909a571160bb4d3c1b8c610
willian017 <willianyuji100@gmail.com>
2025-03-02 15:19:57 -0300

    Implement next and previous slide functionality with improved slide display logic

--- src/pages/index/scripts.js
+++ src/pages/index/scripts.js
@@ -22,11 +22,31 @@ function select(num)
 }
 
 function nextSlide() {
-    select(slideIndex + 1);
+    showSlides2();
 }
 
 function prevSlide() {
-    select(slideIndex - 1);
+    let slides = document.getElementsByClassName("mySlides");
+    slideIndex = slideIndex - 2;
+    if (slideIndex < 1)
+        slideIndex = slides.length - 1;
+    showSlides2();
+}
+
+function showSlides2() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++)
+        slides[i].style.display = "none";  
+    slideIndex++;
+
+    if (slideIndex > slides.length) 
+        slideIndex = 1;    
+    for (i = 0; i < dots.length; i++) 
+        dots[i].className = dots[i].className.replace(" active", "");
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
 }
         
 function showSlides() {
