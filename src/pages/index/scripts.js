1bb846e48da36cda25a65d7c8802ea12206d9418
willian017 <willianyuji100@gmail.com>
2025-03-05 16:33:41 -0300

    Add images for Botucatu 2025 and update styles for responsive design

--- src/pages/index/scripts.js
+++ src/pages/index/scripts.js
@@ -65,3 +65,30 @@ function showSlides() {
     dots[slideIndex-1].className += " active";
     setTimeout(showSlides, 8000);
 }
+
+var height = document.querySelector(".second-article").clientHeight;
+var scrollTop = window.pageYOffset;
+var boolean = true;
+
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 5000;
+
+window.addEventListener('scroll', () => {
+    var scrollTop = window.pageYOffset;
+    if(scrollTop >= height && boolean == true)
+    {
+        boolean = false;
+        valueDisplays.forEach(valueDisplays => {
+            let startValue = 0;
+            let endValue = parseInt(valueDisplays.getAttribute("data-val"));
+            let duration = Math.floor(interval / endValue);
+            let counter = setInterval(function(){
+                startValue += 1;
+                valueDisplays.textContent = startValue;
+                if(startValue == endValue)
+                    clearInterval(counter);
+            }, duration);
+        });
+    }
+});
+
